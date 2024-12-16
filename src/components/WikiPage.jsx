ea63b7a98fc2954ea17e1ded5cc9c9f49bc2355f
alley-wiki <rocker4782@gmail.com>
2024-12-16 11:27:49 +0500

    first commit

--- src/components/WikiPage.jsx
+++ src/components/WikiPage.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Cherry, FileText, Users, Moon, Sun } from 'lucide-react';
 import './WikiPage.css';
-import avatar from "./rocker.png";
+import avatar from "./avatar.png";
 
 export default function WikiPage() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const rockerInfo = {
-    name: "Рокер",
-    nickname: "rocker7003",
-    joinDate: "08.04.2023",
-    age: "16 лет",
-    birthDate: "17.11.08",
-    birthPlace: "Казахстан"
+  const peopleInfo = {
+    name: "Вафф",
+    nickname: "wafflinte_ivory",
+    joinDate: "01.08.2022",
+    age: "18 лет",
+    birthDate: "10.12.06",
+    birthPlace: "Украина",
+    post: "Администратор"
   };
 
   useEffect(() => {
@@ -59,47 +60,56 @@ export default function WikiPage() {
             <div className="main-info section">
               <h2>Основная информация</h2>
               <p>
-                Рокер (rocker7003) - зашел на сервер 8 апреля 2023 года, найдя видео 
-                <a href="/Cherru" className="link"> Черр </a> 
-                в рекомендациях ютуба, в котором ему понравился стиль рисовки, и потом перейдя 
-                по приглашению на сервер из описания видео.
+              Вафф (wafflinte_ivory) - на данный момент владелец дискорд-сервера <a href="https://alley-wiki.github.io/" className="link"> Вишневые Аллеи</a>. Живет в Германии, увлекается рисованием. Самый близкий человек для Вафф - <a href="/Sai" className='link'>Сай</a>.
               </p>
 
-              <h2>Чем занимается</h2>
-              <p>Общается на сервере, делает ии-каверы и редактирует эту википедию.</p>
+              <h2>Попадание на сервер</h2>
+              <p>30 июля 2022 г. один из старых знакомых <a href='/Cherru' className='link'>Черр</a> скинул ссылку на приватный дружеский сервер (где находилась Вафф), ведущий на её видеоролик, с анимацией под названием "punk tactics" (на данный момент анимация недоступна и скрыта). Вафф сразу узнала стиль Черр и перешла по ссылке, подписалась на канал старой подруги, начав активно следить за творчеством авторши.
+
+01.08.2022 на канале <a href='https://www.youtube.com/@cherru_' className='link'>cherru</a> выходит анимация <a href='https://youtu.be/KI3MkN5Hfq4' className='link'>"How did you love"</a>, в описании которой была ссылка-приглашение на её дискорд-сервер. Вафф не раздумывая заходит туда, так как ей хотелось найти новых друзей, попытаться отвлечься от негатива и вновь восстановить общение с самой <a href='/Cherru' className='link'>Черр</a>, что в последствии ей удалось. В дальнейшем, <a href='/Cherru' className='link'>Черр</a> и Вафф вновь начнут хорошо дружить/общаться, Вафф начнёт помогать <a href='/Cherru' className='link'>Черр</a> с управлением сервера, встав на должность администратора.</p>
+
+              <h2>Уходы из сервера</h2>
+              <ul>
+                <li>В один из дней Вафф выходит c сервера из-за страха перед, в прошлом, близким человеком, который очень негативно относился к <a href='/Cherru' className='link'>Черр</a>, но, успокоившись, вскоре возвращается обратно.</li>
+                <li>Один из уходов был связан с тем, что, когда <a href='/Sai' className='link'>Сай</a> и Вафф устроили первый в истории сервера хэллоуинский ивент, с запланированным большим сюжетом, который они прописывали в скрытых каналах на сервере. По просьбе Вафф открыть нужный канал, где была готова следующая часть ивента (на тот момент права администратора у Вафф были "обрезаны"), <a href='/Dunkan' className='link'>Дункан</a> открывает абсолютно все каналы на всеобщее обозрение, что полностью обрушает всю инициативу и желание обоих работать, в ходе чего они уходят из сервера.</li>
+              </ul>
 
               <h2>Интересные факты</h2>
               <ul>
-                <li>Создал эту википедию, но была старая на фэндоме, но её удалили</li>
-                <li>Часто меняет аватарки и никнеймы</li>
-                <li>Иногда притворяется кем-то ради шутки</li>
+                <li>На сервере, из-за шутки <a href='/Cherru' className='link'>Черр</a>, Вафф поставила себе ник "Хагги Ванги", что на долгое время стало её основным никнеймом на сервере.</li>
+                <li>Изначально Вафф использовала по отношению к себе мужские местоимения, притворяясь парнем из-за неприятного опыта в общении. Псевдоним "Вафф" возник случайно и забавно, однажды Вафф написала, что хочет вафли и <a href='/Cherru' className='link'>Черр</a> написала "ваф ли", что случайным образом подтолкнуло на создание данного никнейма.</li>
+                <li>Вафф и <a href='/Cherru' className='link'>Черр</a> были знакомы ещё с 2019-2020-го года, благодаря чатам в Ютубе, которые в скором времени Ютуб и удалил. Активно начали общаться 2020-го года. 20.05.2021 общение прекращается до создания дискорд-сервера <a href="https://alley-wiki.github.io/" className="link"> Вишневые Аллеи</a>.</li>
               </ul>
             </div>
 
             <div className="infobox section">
-              <h2>{rockerInfo.name}</h2>
-              <img src={avatar} alt="Рокер" className="avatar" />
+              <h2>{peopleInfo.name}</h2>
+              <img src={avatar} alt="Вафф" className="avatar" />
               <table>
                 <tbody>
                   <tr>
                     <td>Никнейм</td>
-                    <td>{rockerInfo.nickname}</td>
+                    <td>{peopleInfo.nickname}</td>
                   </tr>
                   <tr>
                     <td>Дата захода на сервер</td>
-                    <td>{rockerInfo.joinDate}</td>
+                    <td>{peopleInfo.joinDate}</td>
                   </tr>
                   <tr>
                     <td>Возраст</td>
-                    <td>{rockerInfo.age}</td>
+                    <td>{peopleInfo.age}</td>
                   </tr>
                   <tr>
                     <td>Дата рождения</td>
-                    <td>{rockerInfo.birthDate}</td>
+                    <td>{peopleInfo.birthDate}</td>
                   </tr>
                   <tr>
                     <td>Место рождения</td>
-                    <td>{rockerInfo.birthPlace}</td>
+                    <td>{peopleInfo.birthPlace}</td>
+                  </tr>
+                  <tr>
+                    <td>Должность</td>
+                    <td>{peopleInfo.post}</td>
                   </tr>
                 </tbody>
               </table>
